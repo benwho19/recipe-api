b3a8c512b4074bbb316eb59acb962e02a8d16f2e
Benjamin <newuser@News-MBP-2.attlocal.net>
2021-02-02 02:52:59 -0800

    added tags endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,10 +1,16 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test@gmail.com', password='testpw1'):
+    """ create a sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
-        """ Test that creating new user with email is successful """
+        """ test that creating new user with email is successful """
         email = 'test123@gmail.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
@@ -35,3 +41,12 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """ test the tag's string representation """
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Pescatarian'
+        )
+
+        self.assertEqual(str(tag), tag.name)
